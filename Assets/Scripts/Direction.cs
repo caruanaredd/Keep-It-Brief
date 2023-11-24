6c170317078ee5d5b360c26cd54d247ade73877f
MasterShofu <74590948+MasterShofu@users.noreply.github.com>
2023-11-24 16:35:22 +0100

    Interaction
    +Added object to detect interaction
    +Added collectible

--- Assets/Scripts/Direction.cs
+++ Assets/Scripts/Direction.cs
@@ -1,7 +1,31 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
 public enum Direction
 {
     Up,
     Down,
     Left,
     Right
+}
+
+public static class DirectionExtensions
+{
+    public static Vector2 ToVector2(this Direction direction)
+    {
+        switch (direction)
+        {
+            case Direction.Up:
+                return Vector2.up;
+            case Direction.Down:
+                return Vector2.down;
+            case Direction.Left:
+                return Vector2.left;
+            case Direction.Right:
+                return Vector2.right;
+            
+        }
+
+        return Vector2.zero;
+    }
 }
